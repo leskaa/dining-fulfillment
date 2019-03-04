cef4d82bc17505c6795c77f1e37e6b537449ba69
Alex Leska <deskmicealex@gmail.com>
2019-03-04 17:01:48 -0600

    now creates json file of ordered data

--- webscraper.py
+++ webscraper.py
@@ -1,13 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 url = 'https://www.ndsu.edu/dining/menu/shortmenu.asp?sName=MENUS+ON+THE+WEB&locationNum=04&locationName=Residence+Dining+Center&naFlag=1'
 
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
+data = {}
 
-breakfastTitle = content.find('div', attrs={"class": "shortmenumeals"}).text
+# Find and add to dictionary in useful order
+divs = content.findAll('div')
+for div in divs:
+    if div.get('class') == ['shortmenumeals']:
+        currentMeal = div.text
+        data[currentMeal] = {}
+    elif div.get('class') == ['shortmenucats']:
+        currentCategory = div.text[3:][:-3]
+        data[currentMeal][currentCategory] = {}
+        itemNumber = 0
+    elif div.get('class') == ['shortmenurecipes']:
+        data[currentMeal][currentCategory][itemNumber] = div.text.replace(
+            '\u00a0', '')
+        itemNumber += 1
 
-menuItems = content.findAll('div', attrs={"class": "shortmenurecipes"})
-for item in menuItems:
-    print(item.text)
+# Save to external file
+with open('./data.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4, sort_keys=True)
