ad62a7340ed4cb2933c72981e3e33f6d1ee129a6
Alex Leska <deskmicealex@gmail.com>
2019-04-29 19:16:57 -0500

    prepare for heroku hosting

--- index.js
+++ index.js
@@ -78,4 +78,8 @@ const expressApp = express().use(bodyParser.json());
 
 expressApp.post('/fulfillment', app);
 
-expressApp.listen(3000);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
