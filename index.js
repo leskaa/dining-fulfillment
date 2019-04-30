d51fede61cb5515e088c44c458e30b0754108d56
Alex Leska <deskmicealex@gmail.com>
2019-04-30 15:16:20 -0500

    switched to async await

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { Client } = require('pg');
+const db = require('./db');
 
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -9,13 +9,6 @@ const cheerio = require('cheerio');
 const { dialogflow, Table } = require('actions-on-google');
 
 const app = dialogflow();
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-client.connect();
 
 app.intent('Default Welcome Intent', conv => {
   conv.ask('Hi, What would you like to know about the NDSU Dining Centers?');
@@ -25,65 +18,50 @@ app.intent('Default Fallback Intent', conv => {
   conv.ask(`I couldn't understand. Can you say that again?`);
 });
 
-app.intent('Get Menu Intent', (conv, params) => {
+app.intent('Get Menu Intent', async (conv, params) => {
 
   let date = new Date(params.date);
   let meal = params.Meal;
   let diningCenter = params.DiningCenter;
 
-  client.query('SELECT * FROM dates WHERE date = $1', [date], (err, res) => {
-    if(err) {
-      console.log(err.stack);
-    } else {
-      if(res.rows.length < 1) {
-        parseDay(date);
-      }
-    }
-    if(meal == '' && diningCenter == '') {
+  const { dateRows } = await db.query('SELECT * FROM dates WHERE date = $1', [date]);
 
-      client.query('SELECT * FROM food-items WHERE date = $1', [date], (err, res) => {
-        if(err) {
-          console.log(err.stack);
-        } else {
-          console.log(res.rows[0]);
-        }
-      });
-      conv.close(`Here is the menu for ${date}.`);
+  if(dateRows.length < 1) {
+    parseDay(date);
+  }
+  
+  if(meal == '' && diningCenter == '') {
 
-    } else if (meal == '') {
+    const { rows } = await db.query('SELECT * FROM food-items WHERE date = $1', [date]);
 
-      client.query('SELECT * FROM food-items WHERE date = $1 AND dining_center = $2', [date, diningCenter], (err, res) => {
-        if(err) {
-          console.log(err.stack);
-        } else {
-          console.log(res.rows[0]);
-        }
-      });
-      conv.close(`Here is ${diningCenter}'s menu for ${date}.`);
+    console.log(rows[0]);
 
-    } else if (diningCenter == '') {
+    conv.close(`Here is the menu for ${date}.`);
 
-      client.query('SELECT * FROM food-items WHERE date = $1 AND meal = $2', [date, meal], (err, res) => {
-        if(err) {
-          console.log(err.stack);
-        } else {
-          console.log(res.rows[0]);
-        }
-      });
-      conv.close(`Here is the ${meal} menu for ${date}.`);
+  } else if (meal == '') {
 
-    } else {
+    const { rows } = await client.query('SELECT * FROM food-items WHERE date = $1 AND dining_center = $2', [date, diningCenter]);
 
-      client.query('SELECT * FROM food-items WHERE date = $1 AND dining_center = $2 AND meal = $3', [date, diningCenter, meal], (err, res) => {
-        if(err) {
-          console.log(err.stack);
-        } else {
-          console.log(res.rows[0]);
-        }
-      });
-      conv.close(`Here is the ${menu} at ${diningCenter} dining center for ${date}.`);
-    }
-  });
+    console.log(rows[0]);
+
+    conv.close(`Here is ${diningCenter}'s menu for ${date}.`);
+
+  } else if (diningCenter == '') {
+
+    const { rows } = await client.query('SELECT * FROM food-items WHERE date = $1 AND meal = $2', [date, meal]);
+
+    console.log(rows[0]);
+
+    conv.close(`Here is the ${meal} menu for ${date}.`);
+
+  } else {
+
+    const { rows } = await client.query('SELECT * FROM food-items WHERE date = $1 AND dining_center = $2 AND meal = $3', [date, diningCenter, meal]);
+
+    console.log(rows[0]);
+
+    conv.close(`Here is the ${menu} at ${diningCenter} dining center for ${date}.`);
+  }
 });
 
 app.intent('Get Hours Intent', (conv, params) => {
@@ -110,36 +88,36 @@ function parseDay(date) {
   parsePage(westUrl, 'west');
 }
 
-function parsePage(pageUrl, diningCenter) {
+async function parsePage(pageUrl, diningCenter) {
 
-  client.query('INSERT INTO dates (date) VALUES ($1)', [new Date()], (err, res) => {
-    if(err) {
-      console.log(err);
-    }
-  });
+  await db.query('INSERT INTO dates (date) VALUES ($1)', [new Date()]);
 
   axios(pageUrl).then(response => {
+
     const html = response.data;
     const $ = cheerio.load(html);
     let currentMeal = '';
     let currentCategory = '';
     let data = {};
     let itemNumber = 0;
+
     $('div').each((i, element) => {
+
       if($(element).attr('class') == 'shortmenumeals') {
+
         currentMeal = $(element).text();
         data[currentMeal] = {};
+
       } else if($(element).attr('class') == 'shortmenucats') {
+
         currentCategory = $(element).text().substring(3, $(element).text().length - 3);
         data[currentMeal][currentCategory] = {};
         itemNumber = 0;
+
       } else if($(element).attr('class') == 'shortmenurecipes') {
+        
         let food = $(element).text().replace('\u00a0', '');
-        client.query('INSERT INTO food-items (food, date, dining_center, meal, menu_category) VALUES ($1, $2,$3, $4, $5', [food, new Date(), diningCenter, currentMeal, currentCategory], (err, res) => {
-          if(err) {
-            console.log(err);
-          }
-        });
+        await client.query('INSERT INTO food-items (food, date, dining_center, meal, menu_category) VALUES ($1, $2,$3, $4, $5', [food, new Date(), diningCenter, currentMeal, currentCategory]);
         itemNumber++;
       }
     });
