70ee740a66cf1655a7c914df0d08fc557855448d
Alex Leska <deskmicealex@gmail.com>
2019-04-29 22:11:42 -0500

    database integrated

--- index.js
+++ index.js
@@ -25,8 +25,69 @@ app.intent('Default Fallback Intent', conv => {
   conv.ask(`I couldn't understand. Can you say that again?`);
 });
 
+app.intent('Get Menu Intent', (conv, params) => {
+
+  let date = new Date(params.date);
+  let meal = params.Meal;
+  let diningCenter = params.DiningCenter;
+
+  client.query('SELECT * FROM dates WHERE date = $1', [date], (err, res) => {
+    if(err) {
+      console.log(err.stack);
+    } else {
+      if(res.rows.length < 1) {
+        parseDay(date);
+      }
+    }
+    if(meal == '' && diningCenter == '') {
+
+      client.query('SELECT * FROM food-items WHERE date = $1', [date], (err, res) => {
+        if(err) {
+          console.log(err.stack);
+        } else {
+          console.log(res.rows[0]);
+        }
+      });
+      conv.close(`Here is the menu for ${date}.`);
+
+    } else if (meal == '') {
+
+      client.query('SELECT * FROM food-items WHERE date = $1 AND dining_center = $2', [date, diningCenter], (err, res) => {
+        if(err) {
+          console.log(err.stack);
+        } else {
+          console.log(res.rows[0]);
+        }
+      });
+      conv.close(`Here is ${diningCenter}'s menu for ${date}.`);
+
+    } else if (diningCenter == '') {
+
+      client.query('SELECT * FROM food-items WHERE date = $1 AND meal = $2', [date, meal], (err, res) => {
+        if(err) {
+          console.log(err.stack);
+        } else {
+          console.log(res.rows[0]);
+        }
+      });
+      conv.close(`Here is the ${meal} menu for ${date}.`);
+
+    } else {
+
+      client.query('SELECT * FROM food-items WHERE date = $1 AND dining_center = $2 AND meal = $3', [date, diningCenter, meal], (err, res) => {
+        if(err) {
+          console.log(err.stack);
+        } else {
+          console.log(res.rows[0]);
+        }
+      });
+      conv.close(`Here is the ${menu} at ${diningCenter} dining center for ${date}.`);
+    }
+  });
+});
+
 app.intent('Get Hours Intent', (conv, params) => {
-  conv.close(`Here is the information for ${params.Meal}`);
+  conv.close(`This feature is not yet supported.`);
 });
 
 function parseDay(date) {
@@ -44,12 +105,19 @@ function parseDay(date) {
   const unionUrl = 'https://www.ndsu.edu/dining/menu/shortmenu.asp?sName=MENUS+ON+THE+WEB&locationNum=10&locationName=Union+Dining+Center&naFlag=1&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate=' + month + '%2F' + day + '%2F' + year;
   const westUrl = 'https://www.ndsu.edu/dining/menu/shortmenu.asp?sName=MENUS+ON+THE+WEB&locationNum=02&locationName=West+Dining+Center&naFlag=1&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate=' + month + '%2F' + day + '%2F' + year;
 
-  parsePage(residenceUrl);
-  parsePage(unionUrl);
-  parsePage(westUrl);
+  parsePage(residenceUrl, 'residence');
+  parsePage(unionUrl, 'union');
+  parsePage(westUrl, 'west');
 }
 
-function parsePage(pageUrl) {
+function parsePage(pageUrl, diningCenter) {
+
+  client.query('INSERT INTO dates (date) VALUES ($1)', [new Date()], (err, res) => {
+    if(err) {
+      console.log(err);
+    }
+  });
+
   axios(pageUrl).then(response => {
     const html = response.data;
     const $ = cheerio.load(html);
@@ -66,7 +134,12 @@ function parsePage(pageUrl) {
         data[currentMeal][currentCategory] = {};
         itemNumber = 0;
       } else if($(element).attr('class') == 'shortmenurecipes') {
-        data[currentMeal][currentCategory][itemNumber] = $(element).text().replace('\u00a0', '');
+        let food = $(element).text().replace('\u00a0', '');
+        client.query('INSERT INTO food-items (food, date, dining_center, meal, menu_category) VALUES ($1, $2,$3, $4, $5', [food, new Date(), diningCenter, currentMeal, currentCategory], (err, res) => {
+          if(err) {
+            console.log(err);
+          }
+        });
         itemNumber++;
       }
     });
