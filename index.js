a91ac607e3926a5c2c770a84ad35b506acdf4945
Alex Leska <deskmicealex@gmail.com>
2019-05-08 22:24:41 -0500

    switched date query to async

--- index.js
+++ index.js
@@ -85,12 +85,13 @@ app.intent('Get Menu Intent', async (conv, params) => {
   const meal = params.Meal;
   const diningCenter = params.DiningCenter;
 
-  db.query('SELECT * FROM dates WHERE date = $1', [date], (err, res) => {
-    console.log(`date query result: ${res}`);
-    if (res.length < 1) {
-      parseDay(date);
-    }
-  });
+  const res = await db.query('SELECT * FROM dates WHERE date = $1', [date]);
+
+  console.log(`date query result: ${res}`);
+
+  if (res.length < 1) {
+    parseDay(date);
+  }
 
   if (meal === '' && diningCenter === '') {
     const { rows } = await db.query(
